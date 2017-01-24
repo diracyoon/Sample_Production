1ae43596f752e3742717d23c6bccee078f47c767
Inseok Yoon <iyoon@cern.ch>
2017-01-24 12:56:21 +0900

    Writing Run_GridPack_To_LHE.sh

--- Macro/Run_GridPack_To_LHE.sh
+++ Macro/Run_GridPack_To_LHE.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+WORK_DIR=`pwd`
+export CMS_PATH="/cvmfs/cms.cern.ch"
+source ${CMS_PATH}/cmsset_default.sh
+export SCRAM_ARCH="slc6_amd64_gcc481"
+
+echo "@@ unzip CMSSW @@"
+tar -zxvf CMSSW_7_1_16_patch2.tar.gz
+cd CMSSW_7_1_16_patch2/src
+
+echo "#### Project Rename ####"
+scram build ProjectRename
